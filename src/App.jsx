f57c0345b31d8a0487f31e272ee9cb2a9bf8b563
boarnerges <olujawo1996@gmail.com>
2025-08-01 10:40:19 +0100

    subcomponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import NewProject from "./components/newProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
